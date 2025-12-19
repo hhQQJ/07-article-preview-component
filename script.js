9526e139851a3581b6358f44a6c9289b203b58ee
hhQQJ <hiqqj@qq.com>
2025-12-19 12:18:03 +0800

    交互完成

--- script.js
+++ script.js
@@ -4,3 +4,9 @@ const shareBtn = document.querySelector(".share-btn");
 shareBtn.addEventListener("click", () => {
   shareWrapper.classList.toggle("is-open");
 });
+
+document.addEventListener("click", (e) => {
+  if (!shareWrapper.contains(e.target)) {
+    shareWrapper.classList.remove("is-open");
+  }
+});
